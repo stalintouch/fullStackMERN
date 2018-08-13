fefba074a56835cc82028bfe9856dba255319f85
Stalintouch <stalintouch1983@gmail.com>
2018-08-13 22:38:57 +0000

    inital commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'therxxxe' })
+})
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0")
