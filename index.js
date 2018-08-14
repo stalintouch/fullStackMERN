e30169c0cecec02886cee2cf52e5bb2bfc7dc9f5
Stalintouch <stalintouch1983@gmail.com>
2018-08-14 20:27:04 +0000

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'therxxxe' })
+  res.send({ hi: 'cool' })
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0")
