0cf6d4914ad8de6b9b99c0036848456b55043e46
stalintouch1983@gmail.com <stalintouch1983@gmail.com>
2018-08-24 21:31:37 +0000

    finished up front end components setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById('root'));
